98c8d9fda3440a13627c31b98ef9284e91534c21
Elvira Lemak <76045352+Elvira9797@users.noreply.github.com>
2023-08-15 15:10:12 +0200

    Revert "Header/future"

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,17 +1,3 @@
 import styled from '@emotion/styled';
 
-export const StyledFooter = styled.footer`
-  background-color: rgb(17, 17, 17);
-  color: ${props => props.theme.colors.secondaryColor};
-`;
-
-export const Container = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 2rem;
-  padding: 1rem 0 1rem 0;
-`;
+export const Div = styled.div``;
