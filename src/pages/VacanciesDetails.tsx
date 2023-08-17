15152729ba0a70aca258332b7dc6392d50472b77
Dmytro Petyshyn <dev6012@gmail.com>
2023-08-17 22:56:54 +0300

    add rendering of vacancy details

--- src/pages/VacanciesDetails.tsx
+++ src/pages/VacanciesDetails.tsx
@@ -1,5 +1,29 @@
+import { useParams } from 'react-router-dom';
+import { vacancies } from '../common/vacanciesArr';
+import SectionContainer from '../components/SectionContainer/SectionContainer';
+import Form from '../components/Form/Form';
+
 const VacanciesDetails = () => {
-  return <div>VacanciesDetails</div>;
+  const { vacancieId } = useParams();
+
+  const getCurrentVacancy = () => {
+    return vacancies.find(vacancy => vacancy.vacancieId === vacancieId);
+  };
+
+  const currentVacancy = getCurrentVacancy();
+
+  return (
+    <SectionContainer>
+      <img src={currentVacancy?.image} alt={currentVacancy?.position} />
+      <p>{currentVacancy?.city}</p>
+      <p>{currentVacancy?.salary}</p>
+      <p>{currentVacancy?.fullInfo}</p>
+      <h1>{currentVacancy?.position}</h1>
+      <div style={{ backgroundColor: '#0f0f0f2f' }}>
+        <Form />
+      </div>
+    </SectionContainer>
+  );
 };
 
 export default VacanciesDetails;
