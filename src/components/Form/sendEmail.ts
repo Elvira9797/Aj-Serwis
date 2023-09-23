f4db07406807ce44e72b2c3e9394db4cae15dd6c
LosOleksandr <losalexander2003@gmail.com>
2023-09-23 11:51:38 +0300

    delete email from form

--- src/components/Form/sendEmail.ts
+++ src/components/Form/sendEmail.ts
@@ -2,6 +2,7 @@ import emailjs from 'emailjs-com';
 import { toast } from 'react-toastify';
 import { FormFields } from './Form';
 import 'react-toastify/dist/ReactToastify.css';
+import i18next from 'i18next';
 
 const EMAIL_JS_USER_ID = 'cpL-disfPacYQD52J';
 const EMAIL_JS_SERVICE_ID = 'service_9yascik';
@@ -11,7 +12,6 @@ emailjs.init(EMAIL_JS_USER_ID);
 export { EMAIL_JS_USER_ID, EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID };
 
 const sendEmail = async (data: FormFields, vacancyName: string) => {
-  console.log('vacancyName: ', vacancyName);
   const emailData = {
     to_email: 'dev6012@meta.ua',
     subject: `${vacancyName}`,
@@ -20,17 +20,16 @@ const sendEmail = async (data: FormFields, vacancyName: string) => {
       Name: ${data.name}
       Surname: ${data.surname}
       Phone: ${data.phone}
-      Email: ${data.email}
       Comment or Review: ${data.comment}
     `,
   };
 
   try {
     await emailjs.send(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, emailData);
-    toast.success('Your credentials was submit successfully');
+    const toastText = i18next.t('main.contactUs.form.toast');
+    toast.success(toastText);
   } catch (error) {
     console.error('Error sending email:', error);
-    toast.error('Ooops, something goes wrong');
   }
 };
 
--- src/components/Form/validationSchema.ts
+++ src/components/Form/validationSchema.ts
@@ -32,11 +32,6 @@ const formSchema = yup.object().shape({
     .min(6, i18next.t('main.contactUs.form.errors.phone.min'))
     .max(18, i18next.t('main.contactUs.form.errors.phone.max'))
     .trim(),
-  email: yup
-    .string()
-    .required(i18next.t('main.contactUs.form.errors.email.required'))
-    .email(i18next.t('main.contactUs.form.errors.email.matches'))
-    .trim(),
   comment: yup
     .string()
     .optional()
